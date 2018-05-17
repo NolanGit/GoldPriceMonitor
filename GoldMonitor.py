1c93c3957753565996816ddfb844ad44c99b0b25
NolanSun <shr1213@live.com>
2018-05-17 21:49:54 +0800

    增加注释

--- GoldMonitor.py
+++ GoldMonitor.py
@@ -18,7 +18,7 @@ def GetPrice():
 	return float(price)
 def Mail(price):
 	my_sender='XXX@qq.com'
-	my_pass = 'XXX'#这个是需要到QQ邮箱里边获取的口令
+	my_pass = 'XXX'#这个是需要到QQ邮箱里边获取的口令，不是QQ邮箱密码
 	my_user='XXXX@XX.XX;XXXX@XX.XX'
 	msg=MIMEText('黄金的价格目前为%s'%(price),'plain','utf-8',)
 	msg['From']=formataddr(["GoldMonitor",my_sender])
@@ -28,7 +28,7 @@ def Mail(price):
 	server.login(my_sender, my_pass)
 	server.sendmail(my_sender,[my_user,],msg.as_string())
 	server.quit()
-	time.sleep(3600)
+	time.sleep(3600)#每小时至多发送一次邮件
 def GetTime():
 	CurrentHour=int(time.strftime('%H',time.localtime(time.time())))
 	CurrentMin=int(time.strftime('%M',time.localtime(time.time())))
@@ -36,8 +36,8 @@ def GetTime():
 	return CurrentTime
 
 CurrentTime=GetTime()
-while 8<CurrentTime<12 or 13.30<CurrentTime<16 or 20<CurrentTime<24:
+while 8<CurrentTime<12 or 13.30<CurrentTime<16 or 20<CurrentTime<24:#仅在国内黄金市场开盘时间前后进行爬取，24点之后休息时间不爬
 	price=GetPrice()
-	if price<266:
+	if price<266:#黄金价格一旦低于266
 		Mail(str(price))
 	time.sleep(10)

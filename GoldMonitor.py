f7110a06ff3c1fd72e62ce6c1abc58d3283c2574
sunhaoran <sunhaoran@inspur.com>
2018-05-18 16:30:43 +0800

    增加微信通知功能

--- GoldMonitor.py
+++ GoldMonitor.py
@@ -5,12 +5,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 import time
+import itchat
 
 def GetPrice():
 	while 1:
 		baseurl='http://www.dyhjw.com/hjtd'
 		r = requests.get(baseurl)
-		time.sleep(5)#避免网速低而加载较慢的情况
+		time.sleep(5)#避免网速低而加载过慢
 		content=r.text
 		soup = BeautifulSoup(content, 'lxml') 
 		divs=soup.find(class_='nom last green')
@@ -18,7 +19,7 @@ def GetPrice():
 			divs=soup.find(class_='nom last red')
 			if divs==None:
 				print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'logging error...')
-				with open('C:\\Users\\XXX\\Documents\\goldLog.txt','a',encoding=UTF-8)as f:
+				with open('C:\\Users\\sunhaoran\\Documents\\goldLog.txt','a',encoding='UTF-8')as f:
 					f.write(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 					f.write(soup)
 					f.write('============================================================')
@@ -34,8 +35,9 @@ def GetPrice():
 	price=divs.get_text()
 	return float(price)
 def MailSender(SenderName,ReceiverAddr,Subject,Content):
-	my_sender='XXXX@qq.com'
-	my_pass = 'XXXXXXXX'#这个是需要到QQ邮箱里边获取的口令，不是QQ邮箱密码
+	my_sender='XXXXX@qq.com'
+	my_pass = 'XXXXX'#这个是需要到QQ邮箱里边获取的口令，不是QQ邮箱密码
+	ReceiverName='Receiver'
 	msg=MIMEText(Content,'plain','utf-8',)
 	msg['From']=formataddr([SenderName,my_sender])
 	msg['To']=formataddr(ReceiverAddr)
@@ -45,27 +47,33 @@ def MailSender(SenderName,ReceiverAddr,Subject,Content):
 	server.sendmail(my_sender,ReceiverAddr,msg.as_string())
 	server.quit()
 	print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'邮件发送成功，一小时后重新获取...')
-	time.sleep(3600)#每小时至多发送一次邮件
+	time.sleep(7200)#每小时至多发送一次邮件
 def GetTime():
 	CurrentHour=int(time.strftime('%H',time.localtime(time.time())))
 	CurrentMin=int(time.strftime('%M',time.localtime(time.time())))
 	CurrentTime=CurrentHour+CurrentMin/100
 	return CurrentTime
-	
+
+itchat.auto_login(hotReload=True)
 print('程序运行中...')
 ReceiverAddr=['XXX@live.com','XXX@qq.com']#填写收件人邮箱
 SenderName='GoldMonitor'
 CurrentTime=GetTime()
-while 8<CurrentTime<12 or 13.30<CurrentTime<16 or 20<CurrentTime<24:#仅在国内黄金市场开盘时间前后进行爬取，24点之后休息时间不爬
-	price=GetPrice()
-	if price<266:#黄金价格一旦低于266
-		Subject='Goldprice'
-		Content='黄金的价格目前为%s,价格较低，可以买入'%(price)
-		MailSender(SenderName,ReceiverAddr,Subject,Content)
-	if price>275:#黄金价格一旦高于275
-		Subject='Goldprice'
-		Content='黄金的价格目前为%s,价格较高，可以卖出'%(price)
-		MailSender(SenderName,ReceiverAddr,Subject,Content)
-	time.sleep(10)#每十秒爬取一次黄金价格
+while 1:
+	while 8<CurrentTime<12 or 13.30<CurrentTime<16 or 20<CurrentTime<24:#仅在国内黄金市场开盘时间前后进行爬取，24点之后休息时间不爬
+		price=GetPrice()
+		if price<265.4:#黄金价格一旦低于265.4
+			Subject='Goldprice'
+			Content='黄金的价格目前为%s,价格较低，可以买入'%(price)
+			itchat.send((time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+Content),'filehelper')
+			MailSender(SenderName,ReceiverAddr,Subject,Content)
+		if price>275:#黄金价格一旦高于275
+			Subject='Goldprice'
+			Content='黄金的价格目前为%s,价格较高，可以卖出'%(price)
+			itchat.send((time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+Content),'filehelper')
+			MailSender(SenderName,ReceiverAddr,Subject,Content)
+		time.sleep(10)#每十秒爬取一次黄金价格
+	print('当前非交易时间,5分钟后重试')
+	time.sleep(300)
 print('程序终止')
 

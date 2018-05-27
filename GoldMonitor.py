11367b084b73dc0c08f8c5b708e0e92c6893d430
NolanSun <shr1213@live.com>
2018-05-27 13:54:10 +0800

    修正时间逻辑的错误

--- GoldMonitor.py
+++ GoldMonitor.py
@@ -18,42 +18,47 @@ def GetTime():
 
 def GetPrice():
     while 1:
-        CurrentTime, CurrentWeek = GetTime()
-        if 8 < CurrentTime < 12 or 13.30 < CurrentTime < 16 or 20 < CurrentTime < 24:  # 仅在国内黄金市场开盘时间前后进行爬取，24点之后休息时间不爬
-            baseurl = 'http://www.dyhjw.com/hjtd'
-            r = requests.get(baseurl)
-            time.sleep(5)  # 避免网速低而加载过慢
-            content = r.text
-            soup = BeautifulSoup(content, 'lxml')
-            divs = soup.find(class_='nom last green')
-            if divs == None:
-                divs = soup.find(class_='nom last red')
+        if CurrentWeek != 0 and CurrentWeek != 6:
+            CurrentTime, CurrentWeek = GetTime()
+            if 8 < CurrentTime < 12 or 13.30 < CurrentTime < 16 or 20 < CurrentTime < 24:  # 仅在国内黄金市场开盘时间前后进行爬取，24点之后休息时间不爬
+                baseurl = 'http://www.dyhjw.com/hjtd'
+                r = requests.get(baseurl)
+                time.sleep(5)  # 避免网速低而加载过慢
+                content = r.text
+                soup = BeautifulSoup(content, 'lxml')
+                divs = soup.find(class_='nom last green')
                 if divs == None:
-                    divs = soup.find(class_='nom last ')
+                    divs = soup.find(class_='nom last red')
                     if divs == None:
-                        print(time.strftime('%Y-%m-%d %H:%M:%S',
-                                            time.localtime(time.time())) + 'logging error...')
-                        with open('C:\\Users\\sunhaoran\\Documents\\goldLog.txt', 'a', encoding='UTF-8')as f:
-                            f.write(time.strftime('%Y-%m-%d %H:%M:%S',
-                                                  time.localtime(time.time())))
-                            f.write(str(soup))
-                            f.write(
-                                '============================================================')
-                        print(time.strftime('%Y-%m-%d %H:%M:%S',
-                                            time.localtime(time.time())) + 'logged...')
-                        print(time.strftime('%Y-%m-%d %H:%M:%S',
-                                            time.localtime(time.time())) + '数据获取失败，五分钟后将重试')
-                        time.sleep(300)
+                        divs = soup.find(class_='nom last ')
+                        if divs == None:
+                            print(time.strftime('%Y-%m-%d %H:%M:%S',
+                                                time.localtime(time.time())) + 'logging error...')
+                            with open('C:\\Users\\sunhaoran\\Documents\\goldLog.txt', 'a', encoding='UTF-8')as f:
+                                f.write(time.strftime('%Y-%m-%d %H:%M:%S',
+                                                      time.localtime(time.time())))
+                                f.write(str(soup))
+                                f.write(
+                                    '============================================================')
+                            print(time.strftime('%Y-%m-%d %H:%M:%S',
+                                                time.localtime(time.time())) + 'logged...')
+                            print(time.strftime('%Y-%m-%d %H:%M:%S',
+                                                time.localtime(time.time())) + '数据获取失败，五分钟后将重试')
+                            time.sleep(300)
+                        else:
+                            break
                     else:
                         break
                 else:
                     break
             else:
-                break
+                print((time.strftime('%Y-%m-%d %H:%M:%S',
+                                     time.localtime(time.time()))) + '当前非交易时间,5分钟后重试')
+                time.sleep(300)
         else:
-            print((time.strftime('%Y-%m-%d %H:%M:%S',
-                                 time.localtime(time.time()))) + '当前非交易时间,5分钟后重试')
-            time.sleep(300)
+            print((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+               ) + '当前为星期%s非交易日，六小时后重试' % (CurrentWeek))
+            time.sleep(21600)
     price = divs.get_text()
     print(time.strftime('%Y-%m-%d %H:%M:%S',
                         time.localtime(time.time())) + '数据获取成功:' + price)
@@ -77,28 +82,23 @@ def MailSender(SenderName, ReceiverAddr, Subject, Content):
     time.sleep(7200)  # 每小时至多发送一次邮件
 
 
-ReceiverAddr = ['XXX@live.com', 'XXX@qq.com']  # 填写收件人邮箱
+ReceiverAddr = ['XXX@live.com', 'XXX@qq.com', 'XXX@outlook.com']  # 填写收件人邮箱
 SenderName = 'GoldMonitor'
 CurrentTime, CurrentWeek = GetTime()
 # itchat.auto_login(hotReload=True)
 print('程序运行中...')
 while 1:
-    if CurrentWeek != 0 and CurrentWeek != 6:
-        price = GetPrice()
-        if price < 265.5:  # 黄金价格一旦低于265.5
-            Subject = 'Goldprice'
-            Content = '黄金的价格目前为%s,价格较低，可以买入' % (price)
+    price = GetPrice()
+    if price < 265.5:  # 黄金价格一旦低于265.5
+        Subject = 'Goldprice'
+        Content = '黄金的价格目前为%s,价格较低，可以买入' % (price)
 # itchat.send((time.strftime('%Y-%m-%d
 # %H:%M:%S',time.localtime(time.time()))+Content),'filehelper')#微信发送消息至文件传输助手
-            MailSender(SenderName, ReceiverAddr, Subject, Content)
-        if price > 280:  # 黄金价格一旦高于275
-            Subject = 'Goldprice'
-            Content = '黄金的价格目前为%s,价格较高，可以卖出' % (price)
-#				itchat.send((time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+Content),'filehelper')
-            MailSender(SenderName, ReceiverAddr, Subject, Content)
-    else:
-        print((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-               ) + '当前为星期%s非交易日，六小时后重试' % (CurrentWeek))
-        time.sleep(21600)
+        MailSender(SenderName, ReceiverAddr, Subject, Content)
+    if price > 280:  # 黄金价格一旦高于275
+        Subject = 'Goldprice'
+        Content = '黄金的价格目前为%s,价格较高，可以卖出' % (price)
+#               itchat.send((time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+Content),'filehelper')
+        MailSender(SenderName, ReceiverAddr, Subject, Content)
     time.sleep(10)  # 每十秒爬取一次黄金价格
-print('程序终止')
+print('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '程序终止')
